e0f4352d9953510d9f025872ad5c5cbb83fa21f7
makoto4423 <52313862+makoto4423@users.noreply.github.com>
2021-10-11 16:49:45 +0800

    commit

--- src/22-map/main.go
+++ src/22-map/main.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var m map[string]int
-	m["key"] = 123
+
 	c := make(map[string]string, 1)
-	c["key"] = "value"
+	c["key"] = "val"
 	fmt.Println(len(c))
+
+	// _ = map[string]int{"key": 123}
+
+	for k, v := range c {
+		fmt.Print("key is " + k + " , value is " + v)
+	}
 
 }
